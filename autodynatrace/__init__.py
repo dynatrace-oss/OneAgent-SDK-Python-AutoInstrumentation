3ba3fdb401fdd41eb68b8b2efc4d98954b25c75c
dlopes7 <davidribeirolopes@gmail.com>
2019-08-25 20:05:08 -0300

    Add custom tracer

--- autodynatrace/__init__.py
+++ autodynatrace/__init__.py
@@ -10,5 +10,6 @@ from .sdk import init as sdk_init
 sdk_init()
 log_init(logging.DEBUG)
 
-from .wrappers import flask, sqlalchemy, urllib3
+from .wrappers import flask, sqlalchemy, urllib3, custom
+dynatrace_custom_tracer = custom.dynatrace_custom_tracer
 logger.debug(f'autdynatrace initialization took {time.time() - start:.2f}s')
