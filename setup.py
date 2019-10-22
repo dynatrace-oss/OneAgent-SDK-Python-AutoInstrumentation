d79f9f585a90ee076d3e66fb7370189aa542fd15
dlopes7 <davidribeirolopes@gmail.com>
2019-10-22 09:28:36 -0300

    Fix typo

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(name='autodynatrace',
-      version='1.0.5',
+      version='1.0.6',
       packages=find_packages(),
+      package_data={'autodynatrace': ['wrappers/*']},
       install_requires=['wrapt>=1.11.2', 'oneagent-sdk>=1.2.1'],
       author='David Lopes',
       author_email='davidribeirolopes@gmail.com',
       description='Auto instrumentation for the OneAgent SDK',
-      long_description='THe autodynatrace package will auto instrument your python apps',
+      long_description='The autodynatrace package will auto instrument your python apps',
       url='https://github.com/dlopes7/autodynatrace',
       classifiers=[
             "Programming Language :: Python :: 3",
