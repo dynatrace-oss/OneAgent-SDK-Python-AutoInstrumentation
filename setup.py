8491c67439f66365a0a15ea6838aa0e6373ec110
dlopes7 <davidribeirolopes@gmail.com>
2020-01-21 19:08:44 -0300

    Change author_email

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="autodynatrace",
-    version="1.0.17",
+    version="1.0.18",
     packages=find_packages(),
     package_data={"autodynatrace": ["wrappers/*"]},
     install_requires=["wrapt>=1.11.2", "oneagent-sdk>=1.2.1", "six==1.13.0"],
     author="David Lopes",
-    author_email="davidribeirolopes@gmail.com",
+    author_email="david.lopes@dynatrace.com",
     description="Auto instrumentation for the OneAgent SDK",
     long_description="The autodynatrace package will auto instrument your python apps",
     url="https://github.com/dlopes7/autodynatrace",
