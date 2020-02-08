d56b26efd84e732c6deaa9b1193fe513a8d0a68c
dlopes7 <davidribeirolopes@gmail.com>
2020-02-08 16:15:06 -0300

    Wrong variable name

--- autodynatrace/wrappers/flask/wrapper.py
+++ autodynatrace/wrappers/flask/wrapper.py
@@ -9,7 +9,7 @@ import socket
 
 def instrument():
     @wrapt.patch_function_wrapper("flask", "Flask.full_dispatch_request")
-    def full_dispatch_request_dynatrace(wrapped, instance, argv, kwargs):
+    def full_dispatch_request_dynatrace(wrapped, instance, args, kwargs):
         try:
             env = flask.request.environ
             method = env.get("REQUEST_METHOD", "GET")
@@ -24,8 +24,8 @@ def instrument():
 
         except Exception as e:
             logger.debug("dynatrace - could not instrument: {}".format(e))
-            return wrapped(*argv, **kwargs)
+            return wrapped(*args, **kwargs)
 
         with sdk.trace_incoming_web_request(wappinfo, url, method, headers=dt_headers):
             logger.debug("dynatrace - full_dispatch_request_dynatrace: {}".format(url))
-            return wrapped(*argv, **kwargs)
+            return wrapped(*args, **kwargs)
