418909028e3f2b1b422e2e350499ff71f7fb5e6c
dlopes7 <davidribeirolopes@gmail.com>
2020-02-12 17:16:48 -0300

    Remove print statements

--- autodynatrace/__init__.py
+++ autodynatrace/__init__.py
@@ -47,11 +47,6 @@ def _on_import_wrapper(lib):
         path = "autodynatrace.wrappers.%s" % lib
         try:
             imported_module = importlib.import_module(path)
-            print(dir(imported_module))
-            print(imported_module.__file__)
-            print(imported_module.__name__)
-            print(imported_module.__path__)
-            print(imported_module.__package__)
             imported_module.instrument()
         except Exception:
             logger.debug("Could not instrument {}".format(lib), exc_info=True)
