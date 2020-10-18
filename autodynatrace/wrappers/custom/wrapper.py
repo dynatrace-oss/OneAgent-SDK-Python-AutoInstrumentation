d512eee2ff5938d8487c1bfea8fcea5e975f3903
dlopes7 <davidribeirolopes@gmail.com>
2020-10-18 10:13:42 -0300

    Rework custom service naming
    Add custom service tests
    Add tox.ini

--- autodynatrace/wrappers/custom/wrapper.py
+++ autodynatrace/wrappers/custom/wrapper.py
@@ -5,18 +5,25 @@ from ...log import logger
 from ...sdk import sdk
 
 
+def generate_service_name(function):
+
+    service_name = function.__module__
+    if hasattr(function, "im_class"):
+        service_name = function.im_class.__name__
+    if hasattr(function, "__qualname__") and "." in function.__qualname__:
+        service_name = function.__qualname__.split(".")[0]
+
+    return service_name
+
+
 def dynatrace_custom_tracer(function):
     @functools.wraps(function)
     def wrapper(*args, **kwargs):
-        function_name = function.__name__
-        module_name = function.__module__
-        if hasattr(function, "im_class"):
-            module_name = function.im_class.__name__
-        if hasattr(function, "__qualname__"):
-            module_name = function.__qualname__.split(".")[0]
-        
-        with sdk.trace_custom_service(function_name, module_name):
-            logger.debug("Custom tracing - {}: {}".format(module_name, function_name))
+        method_name = function.__name__
+        service_name = generate_service_name(function)
+
+        with sdk.trace_custom_service(method_name, service_name):
+            logger.debug("Custom tracing - {}: {}".format(service_name, method_name))
             return function(*args, **kwargs)
 
     return wrapper
---
+++ tests/test_custom.py
@@ -0,0 +1,35 @@
+import sys
+
+import autodynatrace
+import autodynatrace.wrappers.custom.wrapper as custom_wrapper
+
+
+@autodynatrace.trace
+def module_function():
+    return 1
+
+
+class MyClass:
+    def class_method(self):
+        pass
+
+    @staticmethod
+    def static_method(self):
+        pass
+
+
+def test_custom_service_name():
+    my_class = MyClass()
+    assert custom_wrapper.generate_service_name(module_function) == "tests.test_custom"
+    assert custom_wrapper.generate_service_name(my_class.class_method) == "MyClass"
+
+    if sys.version_info[0] == 2:
+        assert custom_wrapper.generate_service_name(my_class.static_method) == "tests.test_custom"
+        assert custom_wrapper.generate_service_name(MyClass.static_method) == "tests.test_custom"
+    else:
+        assert custom_wrapper.generate_service_name(my_class.static_method) == "MyClass"
+        assert custom_wrapper.generate_service_name(MyClass.static_method) == "MyClass"
+
+
+def test_custom_service_instrumentation():
+    assert module_function() == 1
