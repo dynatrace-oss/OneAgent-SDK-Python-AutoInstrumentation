5dcf6f8de8c21a209f3b13f6cec27e6f5a2e342a
dlopes7 <davidribeirolopes@gmail.com>
2020-10-18 14:05:33 -0300

    Wrapped could be any callable, stop using function as its name

--- autodynatrace/wrappers/custom/wrapper.py
+++ autodynatrace/wrappers/custom/wrapper.py
@@ -5,27 +5,27 @@ from ...log import logger
 from ...sdk import sdk
 
 
-def generate_service_name(function):
+def generate_service_name(wrapped):
     if os.environ.get("AUTODYNATRACE_CUSTOM_SERVICE_NAME"):
         return os.environ.get("AUTODYNATRACE_CUSTOM_SERVICE_NAME")
 
-    service_name = function.__module__
-    if hasattr(function, "im_class"):
-        service_name = function.im_class.__name__
-    if hasattr(function, "__qualname__") and "." in function.__qualname__:
-        service_name = function.__qualname__.split(".")[0]
+    service_name = wrapped.__module__
+    if hasattr(wrapped, "im_class"):
+        service_name = wrapped.im_class.__name__
+    if hasattr(wrapped, "__qualname__") and "." in wrapped.__qualname__:
+        service_name = wrapped.__qualname__.split(".")[0]
 
     return service_name
 
 
-def dynatrace_custom_tracer(function):
-    @functools.wraps(function)
+def dynatrace_custom_tracer(wrapped):
+    @functools.wraps(wrapped)
     def wrapper(*args, **kwargs):
-        method_name = function.__name__
-        service_name = generate_service_name(function)
+        method_name = wrapped.__name__
+        service_name = generate_service_name(wrapped)
 
         with sdk.trace_custom_service(method_name, service_name):
             logger.debug("Custom tracing - {}: {}".format(service_name, method_name))
-            return function(*args, **kwargs)
+            return wrapped(*args, **kwargs)
 
     return wrapper
