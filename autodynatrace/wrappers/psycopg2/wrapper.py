6e5de065ec869a027896f2fc6e5c54e939d12c9f
Casey Billman <casey.billman@self.inc>
2023-09-13 11:30:07 -0500

    add proxy cursor object

--- autodynatrace/wrappers/psycopg2/wrapper.py
+++ autodynatrace/wrappers/psycopg2/wrapper.py
@@ -1,8 +1,9 @@
+from abc import ABC
+
 import psycopg2
 import psycopg2.extras
 import wrapt
 import oneagent
-import functools
 
 from ...log import logger
 from ...sdk import sdk
@@ -12,21 +13,22 @@ def instrument():
     def parse_dsn(dsn):
         return {c.split("=")[0]: c.split("=")[1] for c in dsn.split() if "=" in c}
 
-    class DynatraceCursor(psycopg2.extras.DictCursorBase):
-        def __init__(self, *args, **kwargs):
-            self._dynatrace_db_info = kwargs.pop("dynatrace_db_info", None)
-            super(DynatraceCursor, self).__init__(*args, **kwargs)
+    class DynatraceCursor(wrapt.ObjectProxy, ABC):
+
+        def __init__(self, wrapped, dynatrace_db_info):
+            super(DynatraceCursor, self).__init__(wrapped)
+            self._self_dynatrace_db_info = dynatrace_db_info
 
         def execute(self, query, vars=None):
-            if hasattr(self, "_dynatrace_db_info") and self._dynatrace_db_info is not None:
-                with sdk.trace_sql_database_request(self._dynatrace_db_info, "{}".format(query)) as tracer:
+            if hasattr(self, "dynatrace_db_info") and self._self_dynatrace_db_info is not None:
+                with sdk.trace_sql_database_request(self._self_dynatrace_db_info, "{}".format(query)) as tracer:
                     try:
                         logger.debug("Tracing psycopg2 query: '{}'".format(query))
-                        return super(DynatraceCursor, self).execute(query, vars)
+                        return self.__wrapped__.execute(query, vars)
                     finally:
-                        tracer.set_rows_returned(self.rowcount)
+                        tracer.set_rows_returned(self.__wrapped__.rowcount)
 
-            return super(DynatraceCursor, self).execute(query, vars)
+            return self.__wrapped__.execute(query, vars)
 
     class DynatraceConnection(psycopg2.extensions.connection):
         def __init__(self, *args, **kwargs):
@@ -41,11 +43,8 @@ def instrument():
                 ),
             )
 
-            self._dynatrace_cursor = functools.partial(DynatraceCursor, dynatrace_db_info=self._dynatrace_db_info)
-
         def cursor(self, *args, **kwargs):
-            kwargs.setdefault("cursor_factory", self._dynatrace_cursor)
-            return super(DynatraceConnection, self).cursor(*args, **kwargs)
+            return DynatraceCursor(super(DynatraceConnection, self).cursor(*args, **kwargs), self._dynatrace_db_info)
 
     @wrapt.patch_function_wrapper("psycopg2", "connect")
     def dynatrace_connect(wrapped, instance, args, kwargs):
