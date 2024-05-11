0ef67dee039448865b89d0238d84fc2c50587039
dlopes7 <davidribeirolopes@gmail.com>
2024-05-10 19:10:59 -0500

    Fix #87, migrate to pyproject.toml

--- autodynatrace/wrappers/utils.py
+++ autodynatrace/wrappers/utils.py
@@ -1,5 +1,7 @@
 def func_name(view_func):
-    return view_func.__name__
+    if hasattr(view_func, "__name__"):
+        return view_func.__name__
+    return f"{view_func}"
 
 
 def normalize_vendor(name):
