91a120ad40cac3b99457a5547f65180126f25753
theo-fokkinga <120486881+theo-fokkinga@users.noreply.github.com>
2024-08-12 17:39:02 +0200

    Fix FastAPI wrapper for requests from a unix socket

--- autodynatrace/wrappers/fastapi/middleware.py
+++ autodynatrace/wrappers/fastapi/middleware.py
@@ -1,7 +1,7 @@
 import os
 
-from ...sdk import sdk
 from ...log import logger
+from ...sdk import sdk
 
 
 class DynatraceASGIMiddleware:
@@ -9,7 +9,6 @@ class DynatraceASGIMiddleware:
         self.app = app
 
     async def __call__(self, scope, receive, send):
-
         # If this is not an http request, do nothing and return
         if scope["type"] != "http":
             return await self.app(scope, receive, send)
@@ -23,20 +22,31 @@ class DynatraceASGIMiddleware:
 
         dt_tag = headers.get("x-dynatrace")
 
-        host = "{}:{}".format(scope["server"][0], scope["server"][1])
-        url = "{}://{}{}?{}".format(scope["scheme"], host, scope["path"], scope["query_string"].decode())
+        host = ""
+        # scope["server"] is optional, if missing, it defaults to None.
+        # https://asgi.readthedocs.io/en/latest/specs/www.html
+        if scope.get("server") and scope["server"][1]:
+            # [host, port]
+            host = f"{scope['server'][0]}:{scope['server'][1]}"
+        elif scope.get("server"):
+            # [path, None]
+            host = f"{scope['server'][0]}"
+
+        url = f"{scope['scheme']}://{host}{scope['path']}?{scope['query_string'].decode()}"
         method = scope.get("method", "GET")
         app = scope.get("app")
         app_name = app.title if app is not None else "FastAPI"
         root = scope.get("root_path", "/")
 
-        virtual_host = os.environ.get("AUTODYNATRACE_VIRTUAL_HOST", "{}".format(host))
-        app_name = os.environ.get("AUTODYNATRACE_APPLICATION_ID", "{}".format(app_name))
+        virtual_host = os.environ.get("AUTODYNATRACE_VIRTUAL_HOST", f"{host}")
+        app_name = os.environ.get("AUTODYNATRACE_APPLICATION_ID", f"{app_name}")
         context_root = os.environ.get("AUTODYNATRACE_CONTEXT_ROOT", root or "/")
 
         with sdk.create_web_application_info(virtual_host, app_name, context_root) as web_app_info:
-            with sdk.trace_incoming_web_request(web_app_info, url, method, headers=headers, str_tag=dt_tag) as tracer:
-                logger.debug("dynatrace - asgi - Tracing url: '{}' with tag {}".format(url, dt_tag))
+            with sdk.trace_incoming_web_request(
+                web_app_info, url, method, headers=headers, str_tag=dt_tag,
+            ) as tracer:
+                logger.debug(f"dynatrace - asgi - Tracing url: '{url}' with tag {dt_tag}")
 
                 async def wrapped_send(message):
                     if message.get("type") == "http.response.start" and "status" in message:
